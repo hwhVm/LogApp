2ec8a3bbe7f2e9bd0cc59044330c3f368fb409d0
beini <874140704@qq.com>
2018-01-19 17:59:23 +0800

    log util

---
+++ app/src/main/java/beini/com/logapp/MainActivity.java
@@ -0,0 +1,14 @@
+package beini.com.logapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Logger.init("com.beini").
+    }
+}
